96d5fd45ff0e99b910cb6538147b932df63c91c5
TORISOUP <tori.birdstrike@gmail.com>
2024-10-02 12:55:20 +0900

    Test add

--- NdgrClientSharp.Test/NdgrLiveCommentFetcherSpec.cs
+++ NdgrClientSharp.Test/NdgrLiveCommentFetcherSpec.cs
@@ -147,7 +147,6 @@ public sealed class NdgrLiveCommentFetcherSpec
         });
     }
 
-
     [Test]
     public void 最初の取得時にその他のエラー時はリトライせずに諦める()
     {
@@ -253,6 +252,208 @@ public sealed class NdgrLiveCommentFetcherSpec
         });
     }
 
+    [Test]
+    public void 受信中のSegmentが正しく反映される()
+    {
+        var apiClientMock = new Mock<INdgrApiClient>();
+
+        apiClientMock
+            .Setup(x =>
+                x.FetchViewAtNowAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .Returns(new ValueTask<ChunkedEntry.Types.ReadyForNext>(
+                new ChunkedEntry.Types.ReadyForNext
+                {
+                    At = 0
+                }
+            ));
+
+        apiClientMock
+            .Setup(x =>
+                x.FetchChunkedMessagesAsync("segment_1", It.IsAny<CancellationToken>()))
+            .Returns(CreateChunkedMessagesAsync(TimeSpan.FromMilliseconds(100), "1", "2", "3"));
+
+        apiClientMock
+            .Setup(x =>
+                x.FetchChunkedMessagesAsync("segment_2", It.IsAny<CancellationToken>()))
+            .Returns(CreateChunkedMessagesAsync(TimeSpan.FromMilliseconds(100), "4", "5", "6"));
+
+
+        apiClientMock
+            .Setup(x => x.FetchViewAtAsync(It.IsAny<string>(), 0, It.IsAny<CancellationToken>()))
+            .Returns(new[]
+            {
+                new ChunkedEntry()
+                {
+                    Segment = new MessageSegment()
+                    {
+                        Uri = "segment_1",
+                        From = new Timestamp()
+                        {
+                            Seconds = 0 // 時間としてはめちゃくちゃだが処理は進むからOK
+                        }
+                    }
+                },
+                new ChunkedEntry()
+                {
+                    Next = new ChunkedEntry.Types.ReadyForNext()
+                    {
+                        At = 1
+                    }
+                }
+            }.ToAsyncEnumerable());
+
+
+        async IAsyncEnumerable<ChunkedEntry> Create_1(Task waitTask)
+        {
+            await waitTask;
+            yield return new ChunkedEntry()
+            {
+                Segment = new MessageSegment()
+                {
+                    Uri = "segment_1",
+                    From = new Timestamp()
+                    {
+                        Seconds = 0
+                    }
+                }
+            };
+
+            yield return new ChunkedEntry()
+            {
+                Segment = new MessageSegment()
+                {
+                    Uri = "segment_2",
+                    From = new Timestamp()
+                    {
+                        Seconds = 0
+                    }
+                }
+            };
+
+            yield return new ChunkedEntry()
+            {
+                Next = new ChunkedEntry.Types.ReadyForNext()
+                {
+                    At = 2
+                }
+            };
+        }
+
+        async IAsyncEnumerable<ChunkedEntry> Create_2(Task waitTask)
+        {
+            await waitTask;
+            yield return new ChunkedEntry()
+            {
+                Segment = new MessageSegment()
+                {
+                    Uri = "segment_2",
+                    From = new Timestamp()
+                    {
+                        Seconds = 0
+                    }
+                }
+            };
+        }
+
+        var tcs1 = new TaskCompletionSource();
+        var tcs2 = new TaskCompletionSource();
+        apiClientMock
+            .Setup(x => x.FetchViewAtAsync(It.IsAny<string>(), 1, It.IsAny<CancellationToken>()))
+            .Returns(Create_1(tcs1.Task));
+
+        apiClientMock
+            .Setup(x => x.FetchViewAtAsync(It.IsAny<string>(), 2, It.IsAny<CancellationToken>()))
+            .Returns(Create_2(tcs2.Task));
+
+        var fetcher = new NdgrLiveCommentFetcher(apiClientMock.Object);
+
+        // 取得開始
+        fetcher.Connect("test");
+
+        // 最初はSegment1の取得まで進んでいるはず
+        CollectionAssert.AreEqual(fetcher.CurrentReceivingSegments, new[] { "segment_1" });
+
+        // 次に進んでSegment1,2の取得まで進んでいるはず
+        tcs1.SetResult();
+
+        CollectionAssert.AreEqual(fetcher.CurrentReceivingSegments, new[] { "segment_1", "segment_2" });
+
+        // 最後に進んでSegment2のみの取得に進んでいるはず
+        tcs2.SetResult();
+
+        CollectionAssert.AreEqual(fetcher.CurrentReceivingSegments, new[] { "segment_2" });
+    }
+
+    [Test]
+    public void Reconnect時に重複したコメントは受信しない()
+    {
+        var apiClientMock = new Mock<INdgrApiClient>();
+
+        apiClientMock
+            .Setup(x =>
+                x.FetchViewAtNowAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+            .Returns(new ValueTask<ChunkedEntry.Types.ReadyForNext>(
+                new ChunkedEntry.Types.ReadyForNext
+                {
+                    At = 0
+                }
+            ));
+
+        apiClientMock
+            .Setup(x =>
+                x.FetchChunkedMessagesAsync("segment_1", It.IsAny<CancellationToken>()))
+            .Returns(CreateChunkedMessagesAsync(TimeSpan.Zero, "1", "2", "3"));
+
+        async IAsyncEnumerable<ChunkedEntry> CreateSegment(Task waitTask)
+        {
+            yield return new ChunkedEntry()
+            {
+                Segment = new MessageSegment()
+                {
+                    Uri = "segment_1",
+                    From = new Timestamp()
+                    {
+                        Seconds = 0
+                    }
+                }
+            };
+
+            await waitTask;
+
+            yield return new ChunkedEntry()
+            {
+                Next = new ChunkedEntry.Types.ReadyForNext()
+                {
+                    At = 2
+                }
+            };
+        }
+
+        var tcs = new TaskCompletionSource();
+        apiClientMock
+            .Setup(x => x.FetchViewAtAsync(It.IsAny<string>(), 0, It.IsAny<CancellationToken>()))
+            .Returns(CreateSegment(tcs.Task));
+
+        var fetcher = new NdgrLiveCommentFetcher(apiClientMock.Object);
+
+        // 結果の保持用
+        var list = fetcher.OnMessageReceived.Select(x => x.Message.Chat.Content).ToLiveList();
+
+        // 取得開始
+        fetcher.Connect("test");
+
+        // コメントの受信ができている
+        CollectionAssert.AreEqual(new[] { "1", "2", "3" }, list.Order());
+
+        // 再接続する
+        fetcher.Reconnect();
+        tcs.SetResult();
+
+        // 重複したコメントは受信されない
+        CollectionAssert.AreEqual(new[] { "1", "2", "3" }, list.Order());
+    }
+
+
     private async IAsyncEnumerable<ChunkedMessage> CreateChunkedMessagesAsync(TimeSpan delayTime, params string[] texts)
     {
         foreach (var text in texts)
@@ -266,6 +467,10 @@ public sealed class NdgrLiveCommentFetcherSpec
                     {
                         Content = text
                     }
+                },
+                Meta = new ChunkedMessage.Types.Meta()
+                {
+                    Id = text
                 }
             };
         }
